62f6e1143443d0df3b1dd7a0d16a455194596934
Ryan Magoon <mr.magoon5@gmail.com>
2017-11-21 00:10:05 -0500

    add router and wire up initial scene to app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from 'redux'
 import firebase from 'firebase'
 import ReduxThunk from 'redux-thunk'
 import reducers from './reducers'
-import LoginForm from './components/LoginForm'
+import Router from './Router'
 
 class App extends Component {
   componentWillMount() {
@@ -26,9 +26,7 @@ class App extends Component {
 
     return (
       <Provider store={store}>
-        <View style={{ flex: 1, justifyContent: 'center' }}>
-          <LoginForm />
-        </View>
+        <Router />
       </Provider>
     )
   }
