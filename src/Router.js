80a13b2195a0530b70a732c7b2caf8ccfcb51244
Ryan Magoon <mr.magoon5@gmail.com>
2017-11-21 01:02:52 -0500

    Wire up employee create screen

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Scene, Router } from 'react-native-router-flux'
+import { Scene, Router, Actions } from 'react-native-router-flux'
 import LoginForm from './components/LoginForm'
 import EmployeeList from './components/EmployeeList'
+import EmployeeCreate from './components/EmployeeCreate'
 
 const RouterComponent = () => {
   return (
@@ -11,11 +12,17 @@ const RouterComponent = () => {
       </Scene>
       <Scene key="main">
         <Scene
-          onRight={() => console.log('right!!!')}
+          onRight={() => Actions.employeeCreate()}
           rightTitle="Add"
           key="employeeList"
           component={EmployeeList}
           title="Employees"
+          initial
+        />
+        <Scene
+          key="employeeCreate"
+          title="Create Employee"
+          component={EmployeeCreate}
         />
       </Scene>
     </Router>
