334e8eb5d23cd4b37235338d7bf3bc122bd87f43
Ryan Magoon <mr.magoon5@gmail.com>
2017-11-23 15:37:36 -0500

    set default state for shift to Monday

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,10 +1,10 @@
-import { EMPLOYEE_UPDATE } from './types';
+import { EMPLOYEE_UPDATE } from './types'
 
 export const employeeUpdate = ({ prop, value }) => ({
   type: EMPLOYEE_UPDATE,
   payload: { prop, value }
-});
+})
 
 export const employeeCreate = ({ name, phone, shift }) => {
-  console.log(name, phone, shift);
-};
+  console.log(name, phone, shift)
+}
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import { Picker, Text } from 'react-native';
-import { connect } from 'react-redux';
-import { employeeUpdate, employeeCreate } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import React, { Component } from 'react'
+import { Picker, Text } from 'react-native'
+import { connect } from 'react-redux'
+import { employeeUpdate, employeeCreate } from '../actions'
+import { Card, CardSection, Input, Button } from './common'
 
 class EmployeeCreate extends Component {
   onButtonPress() {
-    const { name, phone, shift } = this.props;
+    const { name, phone, shift } = this.props
 
-    this.props.employeeCreate({ name, phone, shift });
+    this.props.employeeCreate({ name, phone, shift: shift || 'Monday' })
   }
 
   render() {
@@ -58,7 +58,7 @@ class EmployeeCreate extends Component {
           <Button onPress={this.onButtonPress.bind(this)}>Create</Button>
         </CardSection>
       </Card>
-    );
+    )
   }
 }
 
@@ -67,22 +67,22 @@ const styles = {
     fontSize: 18,
     paddingLeft: 20
   }
-};
+}
 
 const mapStateToProps = (state) => {
   const {
     name,
     phone,
     shift
-  } = state.employeeForm;
+  } = state.employeeForm
 
   return {
     name,
     phone,
     shift
-  };
-};
+  }
+}
 
 export default connect(mapStateToProps, {
   employeeUpdate, employeeCreate
-})(EmployeeCreate);
+})(EmployeeCreate)
