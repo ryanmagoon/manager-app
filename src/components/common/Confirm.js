3fc60dd437e6fb31fee69c69e180c95196d67439
Ryan Magoon <mr.magoon5@gmail.com>
2017-11-26 18:51:29 -0500

    add styling to Confirm component

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -3,24 +3,46 @@ import { Text, View, Modal } from 'react-native'
 import { CardSection } from './CardSection'
 import { Button } from './Button'
 
-const Confirm = ({ children, onAccept, onDecline, visible }) => (
-  <Modal
-    visible={visible}
-    transparent
-    animationType="slide"
-    onRequestClose={() => {}}
-  >
-    <View>
-      <CardSection>
-        <Text>{children}</Text>
-      </CardSection>
+const styles = {
+  cardSectionStyle: {
+    justifyContent: 'center'
+  },
+  textStyle: {
+    flex: 1,
+    fontSize: 18,
+    textAlign: 'center',
+    lineHeight: 40
+  },
+  containerStyle: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    position: 'relative',
+    flex: 1,
+    justifyContent: 'center'
+  }
+}
 
-      <CardSection>
-        <Button onPress={onAccept}>Yes</Button>
-        <Button onPress={onDecline}>No</Button>
-      </CardSection>
-    </View>
-  </Modal>
-)
+const Confirm = ({ children, onAccept, onDecline, visible }) => {
+  const { containerStyle, textStyle, cardSectionStyle } = styles
+
+  return (
+    <Modal
+      visible={visible}
+      transparent
+      animationType="slide"
+      onRequestClose={() => {}}
+    >
+      <View style={containerStyle}>
+        <CardSection style={cardSectionStyle}>
+          <Text style={textStyle}>{children}</Text>
+        </CardSection>
+
+        <CardSection>
+          <Button onPress={onAccept}>Yes</Button>
+          <Button onPress={onDecline}>No</Button>
+        </CardSection>
+      </View>
+    </Modal>
+  )
+}
 
 export { Confirm }
