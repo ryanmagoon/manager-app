3688e7e14f6cb7b74b353a509d5fa75d3a576d75
Ryan Magoon <mr.magoon5@gmail.com>
2017-11-26 18:46:10 -0500

    write up boilerplate for confirmation modal component

---
+++ src/components/common/Confirm.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Text, View, Modal } from 'react-native'
+import { CardSection } from './CardSection'
+import { Button } from './Button'
+
+const Confirm = ({ children, onAccept, onDecline, visible }) => (
+  <Modal
+    visible={visible}
+    transparent
+    animationType="slide"
+    onRequestClose={() => {}}
+  >
+    <View>
+      <CardSection>
+        <Text>{children}</Text>
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={onAccept}>Yes</Button>
+        <Button onPress={onDecline}>No</Button>
+      </CardSection>
+    </View>
+  </Modal>
+)
+
+export { Confirm }
